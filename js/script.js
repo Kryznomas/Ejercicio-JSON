d17e6d4be62ff8ee5146caeb45a570428cdb68e2
kryznomas <kryztop@gmail.com>
2021-01-20 16:14:39 -0300

    Commit validaciones

--- js/script.js
+++ js/script.js
@@ -1,37 +1,82 @@
-var tabla = [
-    { nombre: "Kryz", edad: 35 },
-    { nombre: "Manuel", edad: 36 },
-    { nombre: "Fernanda", edad: 22 },
-    { nombre: "Alejandra", edad: 22 }
-];
 
-/* window.onload = cargarEventos; */
+/* const formulario  = document.getElementsByTagName('form');
+const nombre = document.getElementById('nombre');
+const nombreError = document.querySelector('#nombre + span.error');
+
+nombre.addEventListener('input', function (event) {
+    // Cada vez que el usuario escribe algo, verificamos si
+    // los campos del formulario son válidos.
+
+    if (nombre.validity.valid) {
+      // En caso de que haya un mensaje de error visible, si el campo
+      // es válido, eliminamos el mensaje de error.
+      nombreError.innerHTML = ''; // Restablece el contenido del mensaje
+      nombreError.className = 'error'; // Restablece el estado visual del mensaje
+    } else {
+      // Si todavía hay un error, muestra el error exacto
+        muestraError();
+    }
+});
+
+formulario.addEventListener('submit', function (event) {
+    // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
+
+    if(!nombre.validity.valid) {
+      // Si no es así, mostramos un mensaje de error apropiado
+        muestraError();
+      // Luego evitamos que se envíe el formulario cancelando el evento
+        event.preventDefault();
+    }
+});
+
+function muestraError() {
+    if(nombre.validity.valueMissing) {
+      // Si el campo está vacío
+      // muestra el mensaje de error siguiente.
+        nombreError.textContent = 'Debe ingresar un nombre.';
+    } else if(nombre.validity.typeMismatch) {
+      // Si el campo no contiene una dirección de correo electrónico
+      // muestra el mensaje de error siguiente.
+        nombreError.textContent = 'El valor introducido debe contener solo letras.';
+        // Establece el estilo apropiado
+    nombreError.className = 'error activo';
+} */
+
+var tabla = [];
+
+window.onload = cargarEventos;
 
 function cargarEventos(){
     
+    document.getElementById("nuevo-usuario").addEventListener("submit", nuevoUsuario, false);
     document.getElementById("mostrar-tabla").addEventListener("click", mostrarTabla, false);
-    document.getElementById("nuevo-usuario").addEventListener("submit", nuevoUsuario, false)
+    
+    
 }
 
 function mostrarTabla() {
+    /* e.preventDefault(); */
     
     var cuerpoTabla = document.getElementById("tabla-datos");
     var tablaCompleta = "";
 
-    for (var i=0; i < tabla.lenght; i++ ) {
+    for (var i=0; i < tabla.length; i++ ) {
         tablaCompleta += "<tr><td>" + tabla[i].nombre + "</td><td>" + tabla[i].edad + "</td></tr>";
 
     }
         cuerpoTabla.innerHTML = tablaCompleta;
     }
 
-    function nuevoUsuario(e){
+    function nuevoUsuario(e) {
         e.preventDefault();
 
-        var nombreIntroducido = element.getElementById("nombre").value;
-        var edadIntroducida = element.getElementById("edad").value;
-        var nuevoUsuario = { nombre: nombreIntroducido, edad: edadIntroducida};
-        tabla.push(nuevoUsuario);
+        var nombreIntroducido = document.getElementById("nombre").value;
+        var edadIntroducida = document.getElementById("edad").value;
+        var nuevoUsuario = { nombre: nombreIntroducido, edad: edadIntroducida };
+        tabla.push(nuevoUsuario)
+
+        document.getElementById("nombre").value = "";
+        document.getElementById("edad").value = "";
 
     }
     
